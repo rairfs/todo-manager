46aecc864f817fc53d7f526c24980e40c789fad0
Raí Rafael <rairafaelss@gmail.com>
2024-05-15 10:15:36 -0300

    Create UserController

---
+++ src/main/java/com/dev/todosimple/controllers/UserController.java
@@ -0,0 +1,51 @@
+package com.dev.todosimple.controllers;
+
+import com.dev.todosimple.models.User;
+import com.dev.todosimple.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = this.userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    @Validated(User.CreateUser.class)
+    public ResponseEntity<Void> create (@Valid @RequestBody User obj) {
+        this.userService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated(User.UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id) {
+        obj.setId(id);
+        this.userService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
